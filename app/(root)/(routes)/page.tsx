56f5fc4edc7f6df639db71acb95ca69987d1fed7
Hansraj8149 <hansrajsaini8149@gmail.com>
2023-10-10 21:10:19 +0530

    user authentication done using clerk

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+
 
 const RootPage = () => {
   return (
     <div>
-      root page
+     <UserButton afterSignOutUrl='/'/>
     </div>
   )
 }
