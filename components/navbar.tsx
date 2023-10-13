e0888f569cc3fc8b1f0b0d3fa1077c8722fccb7d
Hansraj8149 <hansrajsaini8149@gmail.com>
2023-10-13 08:33:15 +0530

    sidebar added successfully

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,6 +6,8 @@ import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./mode-toggle";
+import MobileSidebar from "./mobile-sidebar";
 
 const font = Poppins ({
    weight:"600",
@@ -13,18 +15,19 @@ const font = Poppins ({
 });
 const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="block md:hidden"/>
+        <MobileSidebar/>
           <Link href="/">
             <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}> companion.ai</h1>
           </Link>
       </div>
       <div className="flex items-center gap-x-3">
-         <Button size='sm'>
+         <Button variant='premium' size='sm'>
             Upgrade
             <Sparkles className="h- w-4 fill-white text-white ml-2" />
          </Button>
+         <ModeToggle/>
          <UserButton/>
       </div>
     </div>
